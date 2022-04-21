34f861106b5fa727cb8f16061a3ade54b23365b8
BurakOzdal <burakozda84@gmail.com>
2022-04-21 12:34:09 +0300

    deneme

--- hrms/src/main/java/com/ozdal/hrms/api/controller/JobAdvertisementsController.java
+++ hrms/src/main/java/com/ozdal/hrms/api/controller/JobAdvertisementsController.java
@@ -3,6 +3,7 @@ package com.ozdal.hrms.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.ozdal.hrms.entities.concretes.JobAdvertisement;
 
 @RestController
 @RequestMapping("/api/jobAdvertisments")
+@CrossOrigin
 public class JobAdvertisementsController {
 
 	
